90e7416ee55f8141f9e36e5031410972bb17621b
Aniket  Jha <56987854+BugBear646@users.noreply.github.com>
2021-07-01 17:37:14 +0530

    Delete flair directory

--- flair/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sequence_tagger_model import SequenceTagger, MultiTagger
-from .language_model import LanguageModel
-from .text_classification_model import TextClassifier
-from .text_classification_model import TextPairClassifier
